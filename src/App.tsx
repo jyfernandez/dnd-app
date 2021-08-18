b8dec29ad46d91f30099e5e0afdcf8f0f312080c
jyfernandez <jydfernandez@gmail.com>
2021-08-18 19:22:02 +0800

    Add route for sortable table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+import * as React from "react";
+import SortableTable from "./components/SortableTable";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import "antd/dist/antd.css";
-import SortableTable from "./components/SortableTable";
-
 function App() {
   return (
     <div className="app">
-      <div className="content-container">
-        <SortableTable />
-      </div>
+      <BrowserRouter>
+        <React.Suspense
+          fallback={
+            <div
+              style={{
+                position: "fixed",
+                display: "grid",
+                placeContent: "center",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              Loading&hellip;
+            </div>
+          }
+        >
+          <div className="content-container">
+            <Switch>
+              <Route
+                exact
+                path="/sortable-table"
+                render={() => {
+                  return <SortableTable />;
+                }}
+              />
+            </Switch>
+          </div>
+        </React.Suspense>
+      </BrowserRouter>
+      <SortableTable />
     </div>
   );
 }
